f80ff23c3685246806c7b77e0f67569ee0379c12
Vagner Filho <vagner.martins@sesims.com.br>
2022-03-11 15:01:00 -0400

    quem somos setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
 import Home from '../views/Home.vue'
 import FaleConosco from '../views/fale-conosco/FaleConosco.vue'
+import QuemSomos from '../views/quem-somos/QuemSomos.vue'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -12,6 +13,11 @@ const routes: RouteRecordRaw[] = [
     path: "/fale-conosco",
     name: "FaleConosco",
     component: FaleConosco
+  },
+  {
+    path: "/quem-somos",
+    name: "QuemSomos",
+    component: QuemSomos
   }
 ];
 
---
+++ src/views/quem-somos/QuemSomos.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar :is-transparent="false"/>
+  <Banner
+    path=""
+    figcaption=""
+    img-alt=""
+  >
+    
+  </Banner>
+  <FooterComponent />
+</template>
+
+<script setup lang="ts">
+import Banner from '../../components/general/Banner.vue';
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
