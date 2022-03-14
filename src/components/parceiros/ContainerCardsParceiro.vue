6951be43d737f1fe3145530c93ffb4882cddbd8f
Vagner Filho <vagner.martins@sesims.com.br>
2022-03-14 15:00:27 -0400

    container de parceiros (done)

---
+++ src/components/parceiros/ContainerCardsParceiro.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container-cards-parceiros" :id="'container-parceiros' + containerId">
+    <header>
+      <h1 class="dark-title">{{ tipoDeInstituicao }}</h1>
+      <hr class="w-100">
+    </header>
+    <main>
+      <!-- TODO: fix card distance based on amount of cards -->
+      <CardParceiro v-for="(parceiro, index) in parceiros" :key="parceiro.parceiroId" 
+        :card-parceiro="parceiro"
+      />
+    </main>
+  </section>
+</template>
+
+<script setup lang="ts">
+import CardParceiro from './CardParceiro.vue';
+
+  const props = defineProps<{
+    tipoDeInstituicao: string
+    parceiros: {
+      hasLogo: boolean,
+      logo: string,
+      logoAlt: string,
+      parceiroId: number
+    }[]
+    containerId: number
+  }>()
+</script>
+
+<style scoped lang="scss">
+  .container-cards-parceiros {
+    header {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      margin-top: 70px;
+      margin-bottom: 10px;
+      h1 {
+        font-size: 1.5rem;
+      }
+    }
+    main{
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      grid-template-rows: 220px;
+      gap: 20px 9%;
+    }
+  }
+  @media (max-width: 1031px) {
+    .container-cards-parceiros {
+      main{
+        column-gap: 5%;
+      }
+    }
+  }
+  @media (max-width: 576px) {
+    .container-cards-parceiros {
+      main {
+        justify-items: center;
+      }
+    }
+  }
+</style>
--- src/views/parceiros/Parceiros.vue
+++ src/views/parceiros/Parceiros.vue
@@ -1,11 +1,119 @@
 <template>
-    <router-link to="/parceiros/id">clica aqui</router-link>
+  <!-- <router-link to="/parceiros/id">clica aqui</router-link> -->
+  <NavBar 
+    :is-transparent="false"
+  />
+  <section class="parceiros ghp">
+    <ContainerCardsParceiro v-for="(container, index) in parceirosContainers" :key="index"
+      :tipo-de-instituicao="container.tipoInstituicao"
+      :parceiros="container.parceiros"
+      :container-id="index"
+    />
+  </section>
+  <FooterComponent />
 </template>
 
 <script setup lang="ts">
+import NavBar from '../../components/general/NavBar.vue';
+import FooterComponent from '../../components/general/FooterComponent.vue';
+import ContainerCardsParceiro from '../../components/parceiros/ContainerCardsParceiro.vue';
+import { reactive } from 'vue';
 
+  const parceirosContainers = reactive([
+    {
+      tipoInstituicao: 'UNIVERSIDADES',
+      parceiros: [
+        {
+          hasLogo: true,
+          logo: '/src/assets/home/carousel/estacio.png',
+          logoAlt: 'estacio',
+          parceiroId: 1
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 2
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 3
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 4
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 5
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 6
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 7
+        },
+      ]
+    },
+    {
+      tipoInstituicao: 'EMPRESAS',
+      parceiros: [
+        {
+          hasLogo: true,
+          logo: '/src/assets/home/carousel/estacio.png',
+          logoAlt: 'estacio',
+          parceiroId: 1
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 2
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 3
+        }
+      ]
+    },
+    {
+      tipoInstituicao: 'STARTUPS',
+      parceiros: [
+        {
+          hasLogo: true,
+          logo: '/src/assets/home/carousel/estacio.png',
+          logoAlt: 'estacio',
+          parceiroId: 1
+        },
+        {
+          hasLogo: false,
+          logo: '...',
+          logoAlt: 'parceiro',
+          parceiroId: 2
+        }
+      ]
+    }
+  ])
 </script>
 
 <style scoped lang="scss">
-
+  section.parceiros {
+    background-color: #f6f6f6;
+    padding-top: 3rem !important;
+    padding-bottom: 3rem !important;
+  }
 </style>
